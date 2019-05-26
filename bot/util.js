b915414a4c9b8381dfc386c7eeb705fab20fcb5a
Fiona <fiona.greenwood.sheridan@gmail.com>
2019-05-26 13:44:11 +1000

    Renamed webhook stuff

--- bot/util.js
+++ bot/util.js
@@ -5,10 +5,10 @@ const config = require("./config.json")
 module.exports = {
 
   //Webhook Finder
-  async webhookCheck(client, msg) {
-    var webhooks = await msg.guild.fetchWebhooks()
+  async getWebhook(client, channel) {
+    var webhooks = await channel.guild.fetchWebhooks()
 		var hook
-		webhooks = webhooks.filter(hook => hook.channelID === msg.channel.id)
+		webhooks = webhooks.filter(hook => hook.channelID === channel.id)
 		if(webhooks.find(hook => hook.owner.id === client.user.id) == null){
 			hook = await msg.channel.createWebhook("CinnamonRP")
 		} else {
