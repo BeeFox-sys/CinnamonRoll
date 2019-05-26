16c119eb486fdb52947814a76d08c899ea399623
xBelladonna <isabelladonnamoore@outlook.com>
2019-05-26 22:12:22 +0930

    Add basic info message
    Add a basic information message and invite link to the official support server

--- bot/commands/info.js
+++ bot/commands/info.js
@@ -2,16 +2,17 @@ const utils = require('../util');
 
 module.exports = {
   name: 'info',
-  decription: 'Generates an invite link for the bot',
+  decription: 'Shows information about the bot',
   perms: ['invite', 'github', 'support', 'server'],
   guildOnly: false,
   args: false,
   usage:'',
   async execute(client, guildSettings, msg, args) {
+    // Fetch the bot info and get the client ID
     const oauth_app = await client.fetchApplication();
     const client_id = oauth_app.id;
     /*
-    Give the bot permissions integer '536996928' which allows the following permissions:
+    Give the bot permissions integer "536996928" which allows the following permissions:
 
     Manage webhooks - so the bot can make [NPC] posts
     View channels - so the bot can read text channels (and see voice channels) throughout the server
@@ -22,16 +23,20 @@ module.exports = {
     Read messages history - might need this later on for message lookup/context-aware stuff
     Add reactions - let the bot react to messages e.g. for controlling embed panels
     */
-    const permissions = '536996928';
+    const permissions = "536996928";
 
     // Construct the URL with proper client ID and permissions integer
     const inviteUrl = `https://discordapp.com/oauth2/authorize?client_id=${client_id}&scope=bot&permissions=${permissions}`;
     //Misc links
-    const githubUrl = "https://github.com/PlatypodeCode/CinnamonRP"
+    const githubUrl = "https://github.com/PlatypodeCode/CinnamonRP";
     // Generate Embed
+    const infoMessage = "CinnamonRP is a bot designed for roleplay on Discord. It allows you to create characters and set up message proxying, set scenes and define locations, post as NPCs, roll dice and more.\n\nType \`!!help\` for a list of commands and \`!!help [command]\` to find out how they work!\n\nWe also have a support server for help, announcements, discussion, suggestions, etc";
+    const serverUrl = "https://discord.gg/NjrSpBY";
     const embed = utils.passEmbed()
+      .addField("CinnamonRP", infoMessage)
       .addField("Add the bot!",`[Click here to add CinnamonRP to your server](${inviteUrl})`)
-      .addField("See the code!",`[Click here to view the github for CinnamonRP](${githubUrl})`)
+      .addField("See the code!",`[Click here to view the GitHub for CinnamonRP](${githubUrl})`)
+      .addField("Get help!", `[Click here to join our support server](${serverUrl})`);
     // Finally, send the message with the invite link
     return msg.channel.send(embed);
   },
