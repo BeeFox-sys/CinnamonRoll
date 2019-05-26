ef94e66cd683bd99ec3c6207194ebfa4150a9fd2
xBelladonna <isabelladonnamoore@outlook.com>
2019-05-27 02:41:17 +0930

    Display prefix from config in presence

--- bot/main.js
+++ bot/main.js
@@ -41,10 +41,10 @@ client.once('ready', async () => {
   console.log(`${client.guilds.size} servers`);
   // console.log(`${client.shard.count} shards`); // for future use once sharding becomes necessary
   if(client.guilds.size < 2) {
-    client.user.setActivity(`!!help`, { type: 'LISTENING'});
+    client.user.setActivity(`${config.prefix}help`, { type: 'LISTENING'});
   }
   else {
-    client.user.setActivity(`!!help | in ${client.guilds.size} servers`, { type: 'LISTENING'});
+    client.user.setActivity(`${config.prefix}help | in ${client.guilds.size} servers`, { type: 'LISTENING'});
   }
 });
 
