26195ca117a5ca0b71cab234ce3e59b51b157da1
Fiona <fiona.greenwood.sheridan@gmail.com>
2019-05-30 01:01:04 +1000

    Whoops

--- scripts/dump-db.sh
+++ scripts/dump-db.sh
@@ -1,12 +1,3 @@
-<<<<<<< current
-#!/bin/bash
-
-## Usage: ./dump-db.sh
-
-
-docker-compose exec -T db mongodump --db CinnamonRoll --gzip --out /backup/$FILENAME
-docker-compose exec -T db tar cvfz /backup/$FILENAME.tar /backup/$FILENAME
-=======
 #!/bin/bash
 
 ## Usage: ./dump-db.sh
@@ -15,5 +6,4 @@ FILENAME=CinnamonRoll-db-$(date -u +"%Y-%m-%dT%H-%M-%S")
 
 docker-compose exec -T db mongodump --db CinnamonRoll --gzip --out /backup/$FILENAME
 docker-compose exec -T db tar cvfz /backup/$FILENAME.tar /backup/$FILENAME
->>>>>>> before discard
 docker-compose exec -T db rm -rf /backup/$FILENAME
