44525eb6caa5b15905e6b4fd60e5d9537b08f626
Fiona <fiona.greenwood.sheridan@gmail.com>
2019-05-29 16:21:30 +1000

    Updated settings to use a switch statement

--- bot/commands/settings.js
+++ bot/commands/settings.js
@@ -41,74 +41,90 @@ Sets the server's game name`,
 				return msg.channel.send(embed)
 			}
 
+			switch (args[0]) {
+				case "prefix":
+					if(args.length < 2){
+						return msg.channel.send(utils.errorEmbed('You must supply a prefix to change to'))
+					}
+					guildSettings.prefix = args[1]
+					return guildSettings.save((err, doc) => {
+						if(err){
+							console.log(err)
+							return msg.channel.send(utils.errorEmbed("There was an error trying to execute that command!"))
+						} else {
+							return msg.channel.send(utils.passEmbed(`Prefix changed to \`${doc.prefix}\``))
+						}
+					})
+				break;
 
-			//prefix subcommand
-			else if (args[0] == 'prefix') {
-				if(args.length < 2){
-					return msg.channel.send(utils.errorEmbed('You must supply a prefix to change to'))
-				}
-				guildSettings.prefix = args[1]
-				return guildSettings.save((err, doc) => {
-		      if(err){
-		        console.log(err)
-		        return msg.channel.send(utils.errorEmbed("There was an error trying to execute that command!"))
-		      } else {
-		        return msg.channel.send(utils.passEmbed(`Prefix changed to \`${doc.prefix}\``))
-		      }
-		    })
+
+				case "role":
+					var guildRoles = msg.guild.roles
+					var roleName = args.slice(2).join(" ")
+					var role = guildRoles.find(roleFind => roleFind.name === roleName);
+					switch (args[1]) {
+						case "add":
+							if(role == null) return msg.channel.send(utils.errorEmbed("That role was not found"))
+							if(guildSettings.admin.find((roleID)=>{return roleID == role.id})) return msg.channel.send(utils.errorEmbed("That role is already a manager!"))
+							guildSettings.admin.push(role.id)
+							return guildSettings.save((err, doc) => {
+								if(err){
+									console.log(err)
+									return msg.channel.send(utils.errorEmbed("There was an error trying to execute that command!"))
+								} else {
+									return msg.channel.send(utils.passEmbed(role.name+" has been added to manager roles"))
+								}
+							})
+						break;
+
+						case "remove":
+							if(role == null) return msg.channel.send(utils.errorEmbed("That role was not found"))
+							if(!guildSettings.admin.find((roleID)=>{return roleID == role.id})) return msg.channel.send(utils.errorEmbed("That role isn't a manager!"))
+							pos = guildSettings.admin.indexOf(role.id)
+							guildSettings.admin.splice(pos, 1)
+							return guildSettings.save((err, doc) => {
+					      if(err){
+					        console.log(err)
+					        return msg.channel.send(utils.errorEmbed("There was an error trying to execute that command!"))
+					      } else {
+					        return msg.channel.send(utils.passEmbed(role.name+" has been removed from manager roles"))
+					      }
+					    })
+						break;
+
+						default:
+							return msg.channel.send(utils.errorEmbed("That is not a valid option\nValid options are `add` and `remove`"))
+
+					}
+				break;
+
+
+
+				case "name":
+				case "rename":
+					if(args.length > 1){
+						guildSettings.gameName = args.slice(1).join(" ")
+						var response = `Name set to **${guildSettings.gameName}**`
+					} else {
+						guildSettings.gameName = ""
+						var response = "Name Cleared"
+					}
+					return guildSettings.save((err, doc) => {
+			      if(err){
+			        console.log(err)
+			        return msg.channel.send(utils.errorEmbed("There was an error trying to execute that command!"))
+			      } else {
+			        return msg.channel.send(utils.passEmbed(response))
+			      }
+			    })
+				break;
+
+
+
+				default:
+					return msg.channel.send(utils.errorEmbed('That is not a subcommand'))
 			}
-
-
-			//Manager Roles
-			else if (args[0] == "role" && (args[1] == "add" || args[1] == "remove")) {
-				const guildRoles = msg.guild.roles
-				roleName = args.slice(2).join(" ")
-				role = guildRoles.find(roleFind => roleFind.name === roleName);
-				response = ""
-				if(role == null) return msg.channel.send(utils.errorEmbed("That role was not found"))
-				if (args[1] == "add"){
-					if(guildSettings.admin.find((roleID)=>{return roleID == role.id})) return msg.channel.send(utils.errorEmbed("That role is already a manager!"))
-					guildSettings.admin.push(role.id)
-					response = role.name+" has been added to manager roles"
-				}
-				else if(args[1] == "remove"){
-					if(!guildSettings.admin.find((roleID)=>{return roleID == role.id})) return msg.channel.send(utils.errorEmbed("That role isn't a manager!"))
-					pos = guildSettings.admin.indexOf(role.id)
-					guildSettings.admin.splice(pos, 1)
-					response = role.name+" has been removed from manager roles"
-				}
-				return guildSettings.save((err, doc) => {
-		      if(err){
-		        console.log(err)
-		        return msg.channel.send(utils.errorEmbed("There was an error trying to execute that command!"))
-		      } else {
-		        return msg.channel.send(utils.passEmbed(response))
-		      }
-		    })
-			}
-			//name
-			else if (args[0] == "name") {
-				if(args.length > 2){
-					guildSettings.gameName = args.slice(1).join(" ")
-					response = `Name set to **${guildSettings.gameName}**`
-				} else {
-					guildSettings.gameName = ""
-					response = "Name Cleared"
-				}
-				return guildSettings.save((err, doc) => {
-		      if(err){
-		        console.log(err)
-		        return msg.channel.send(utils.errorEmbed("There was an error trying to execute that command!"))
-		      } else {
-		        return msg.channel.send(utils.passEmbed(response))
-		      }
-		    })
-
-			}
-
-
-			return msg.channel.send(utils.errorEmbed('That is not a subcommand'))
 		}
     return msg.channel.send(utils.errorEmbed('You do not have the correct permissions'))
 	}
-};
+}
