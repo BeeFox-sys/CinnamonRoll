5268d47d98f99229da07e4ce9fc5a7897e1a5790
Fiona <fiona.greenwood.sheridan@gmail.com>
2019-05-29 16:37:08 +1000

    Fun Things 😉

---
+++ bot/commands/say.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const utils = require('../util.js')
+const config = require('../config.json');
+
+module.exports = {
+	name: 'say',
+	aliases: [],
+	description: 'Owner Only fun times',
+	hidden: true,
+	args: false,
+	argsMin: 0,
+	usage: '',
+	example: '',
+	async execute(client, guildSettings, msg, args) {
+    if(msg.member.id == config.owner.id){
+			var message = args.join(" ")
+			await msg.channel.send(message)
+			return await msg.delete()
+		}
+	},
+};
