b05bbbda4c609db0edb14f883672c0b913905e8d
Fiona <fiona.greenwood.sheridan@gmail.com>
2019-05-30 13:44:07 +1000

    Export Command Complete

--- bot/commands/export.js
+++ bot/commands/export.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const utils = require('../util.js');
 const mongoose = require('mongoose');
+const fs = require("fs")
 
 
 module.exports = {
@@ -33,19 +34,24 @@ async function exportObject(id, array, msg) {
 	//Get object to export
 	var exportObj = await utils.findObjInArray(id, array)
 	if(!exportObj) return await msg.channel.send(utils.errorEmbed(`${id} does not exist`))
+	var exportMsg = await msg.channel.send(utils.warnEmbed("Exporting... This may take a second"))
 	//remove database info
 	exportObj._id = undefined
 	exportObj.guild = undefined
 	exportObj.owner = undefined
 	exportObj.__v = undefined
-	//return object
-	//TODO: Create a file and return it
-	try{
-		await msg.author.send(utils.passEmbed(`\`\`\`${exportObj.toString()}\`\`\``).setTitle(`Export for ${exportObj.name}`))
-		return msg.channel.send(utils.passEmbed("Sent you a DM!"))
-	} catch(e) {
-		return await msg.channel.send(utils.passEmbed(`\`\`\`${exportObj.toString()}\`\`\``).setTitle(`Export for ${exportObj.name}`).setFooter("Couldn't send this in a DM, so I sent it in main chat"))
-	}
-	
+	var writeStream = await fs.createWriteStream(`${exportObj.name}.json`)
+	await writeStream.write(exportObj.toString())
+	await writeStream.end()
+	var attachment = await new Discord.Attachment(`${exportObj.name}.json`)
 
+	try{
+		await msg.author.send(utils.passEmbed(`Here is your export`).setTitle(`Export for ${exportObj.name}`))
+		await msg.author.send(attachment)
+		await msg.channel.send(utils.passEmbed("Sent you a DM!"))
+	} catch(e) {
+		await msg.channel.send(utils.passEmbed().setTitle(`Export for ${exportObj.name}`).setFooter("I couldn't send this in a DM, so I sent it in chat"))
+		await msg.channel.send(attachment)
+	}
+	await fs.unlink(`${exportObj.name}.json`, (err) => {if (err) throw err;})
 }
