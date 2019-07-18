463dbfe715ef8a9d169541d7896ad3daaf459e84
xBelladonna <isabelladonnamoore@outlook.com>
2019-07-18 11:36:35 +0930

    Add error to quoteFinder util
    If the input array to the quoteFinder util is empty, we throw a useful error

--- bot/util.js
+++ bot/util.js
@@ -131,7 +131,7 @@ utils = {
     },
 
   quoteFinder(array){
-    if(array == undefined) return array
+    if (!array.length) throw "Input array to quoteFinder is empty!";
     if(array[0].startsWith('"') == false) return array
     var start = null
     var end = null
